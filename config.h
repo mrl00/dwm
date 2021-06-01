a6c84ba0052c9e178c540d766e3f5dd552a5323b
mrl00 <m2l_0@outlook.com>
2021-06-01 14:28:04 -0300

    sdfsd

--- config.h
+++ config.h
@@ -1,24 +1,25 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     	/* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=12";
+static const unsigned int borderpx				= 2;	/* border pixel of windows */
+static const unsigned int snap      			= 32; /* snap pixel */
+static const unsigned int systraypinning 	= 0;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing 	= 2;  /* systray spacing */
+static const int systraypinningfailfirst 	= 1;  /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        			= 1;  /* 0 means no systray */
+static const int showbar            			= 1;  /* 0 means no bar */
+static const int topbar             			= 1;  /* 0 means bottom bar */
+static const char *fonts[]          			= { "monospace:size=10" };
+static const char dmenufont[]       			= "monospace:size=12";
 
-static const char col_gray1[]         = "#282a36";
-static const char col_gray2[]         = "#282a36"; /* border color unfocused windows */
-static const char col_gray3[]         = "#96b5b4";
-static const char col_gray4[]         = "#d7d7d7";
-static const char col_cyan[]          = "#924441"; /* border color focused windows and tags */
-static const char col_green[]		  = "#1aff1a";
-static const char col_orange[]		  = "#ff5733";
+static const char col_gray1[]         		= "#282a36";
+static const char col_gray2[]         		= "#282a36"; /* border color unfocused windows */
+static const char col_gray3[]         		= "#96b5b4";
+static const char col_gray4[]         		= "#d7d7d7";
+static const char col_cyan[]          		= "#924441"; /* border color focused windows and tags */
+static const char col_green[]		  				= "#1aff1a";
+static const char col_orange[]		  			= "#ff5733";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -69,8 +70,16 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+//Volume
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "1", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "1", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "1", "toggle",  NULL };
+
 
 static Key keys[] = {
+	{ 0,                       		XF86XK_AudioLowerVolume, 	spawn, {.v = downvol } },
+	{ 0,                       		XF86XK_AudioMute, 				spawn, {.v = mutevol } },
+	{ 0,                       		XF86XK_AudioRaiseVolume, 	spawn, {.v = upvol   } },
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
