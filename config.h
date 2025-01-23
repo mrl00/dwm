247cc36c9650175c4e5bdcba2ae3899c946ba335
mrl00 <m2l_0@outlook.com>
2025-01-23 18:52:49 -0300

    change font

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const int showsystray        			= 1;  /* 0 means no systray */
 static const int showbar            			= 1;  /* 0 means no bar */
 static const int topbar             			= 1;  /* 0 means bottom bar */
 static const char *fonts[]          			= { "monospace:size=11" };
-static const char dmenufont[]       			= "FiraCode Nerd Font:size=12";
+static const char dmenufont[]       			= "monospace:size=11";
 
 static const char col_gray1[]         		= "#282a36";
 static const char col_gray2[]         		= "#282a36"; /* border color unfocused windows */
@@ -76,9 +76,9 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 //Volume
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "1", "+5%",     NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "1", "-5%",     NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "1", "toggle",  NULL };
+static const char *upvol[]   = { "/usr/bin/pamixer", "-i", "5", NULL,     NULL };
+static const char *downvol[] = { "/usr/bin/pamixer", "-d", "5", NULL,     NULL };
+static const char *mutevol[] = { "/usr/bin/pamixer", "-t",   NULL, NULL,  NULL };
 
 
 static Key keys[] = {
